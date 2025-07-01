7f914cbd0e2110de3055ec302e0b4a05a1d315b9
Keltoum Boukra <keltoum.boukra96@gmail.com>
2025-07-01 12:51:24 -0500

    Update dspy tool script to kick off implementation

--- dspy_implementation/dspy_implementation.py
+++ dspy_implementation/dspy_implementation.py
@@ -1,5 +1,13 @@
 import dspy
+
 llm = dspy.LM('ollama_chat/llama3.2', 
   api_base='http://localhost:11434', 
   api_key='', temperature = 0.3)
 dspy.configure(lm=llm)
+
+simple_model = dspy.Predict("question -> answer: int")
+
+simple_model(
+  question="""I have 5 different balls and I randomly select 4. 
+    How many possible combinations of the balls I can get?"""
+)
